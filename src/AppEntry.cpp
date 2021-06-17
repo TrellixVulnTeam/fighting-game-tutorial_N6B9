2d9daa9c476d2536d42bedf6b010d6cbdfa7babd
Denis Ponyakov <nexons.97@gmail.com>
2021-06-18 00:49:09 +0300

    Added OpenGL support, created entry points for frame update and render

--- src/AppEntry.cpp
+++ src/AppEntry.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <GL/glew.h>
+
+SDL_Window* sdlWindow = nullptr;
+SDL_GLContext glContext = nullptr;
+
+void update_game_state()
+{
+
+}
+
+void render_frame()
+{
+	// Clear render target
+	glClearColor(0.f, 0.5f, 1.f, 1.f);
+	glClearDepth(1.f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Present drawn image to the window surface
+	SDL_GL_SwapWindow(sdlWindow);
+}
 
 int main()
 {
@@ -11,7 +31,23 @@ int main()
 	}
 
 	// Create non-resizable window with SDL support
-	SDL_Window* sdlWindow = SDL_CreateWindow("Fighting Game", 50, 50, 1600, 900, SDL_WINDOW_OPENGL);
+	sdlWindow = SDL_CreateWindow("Fighting Game", 50, 50, 1600, 900, SDL_WINDOW_OPENGL);
+	if (sdlWindow)
+	{
+		// Create OpenGL context for rendering
+		glContext = SDL_GL_CreateContext(sdlWindow);
+		if (!glContext)
+		{
+			std::cerr << "Failed to create OpenGL context!" << std::endl;
+			return 1;
+		}
+		else
+		{
+			// Apply create OpenGL context and initialize OpenGL extensions using glew
+			SDL_GL_MakeCurrent(sdlWindow, glContext);
+			glewInit();
+		}
+	}
 
 	// Enter SDL event processing loop
 	bool shutdownRequested = false;
@@ -30,6 +66,16 @@ int main()
 		}
 
 		// Run application logic for this frame
+		update_game_state();
+
+		// Perform rendering of the frame according to new state
+		render_frame();
+	}
+
+	// Delete OpenGL context
+	if (glContext)
+	{
+		SDL_GL_DeleteContext(glContext);
 	}
 
 	// Shutdown SDL library
